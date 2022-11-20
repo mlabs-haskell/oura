a7387afd28ca1fffd8093b2f44c9fc1154d9677b
Franco Cortesi <cortesifranco3@gmail.com>
2022-11-19 21:45:52 -0300

    nix build with webhook support

--- flake.nix
+++ flake.nix
@@ -2,21 +2,31 @@
   inputs = {
     crane.url = "github:ipetkov/crane";
     utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-22.05";
   };
 
   outputs = {
     self,
     crane,
     utils,
+    nixpkgs,
     ...
   }: let
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux"];
+    pkgs = import nixpkgs {system= "x86_64-linux";};
   in
     utils.lib.eachSystem supportedSystems
     (
       system: {
         packages.oura = crane.lib.${system}.buildPackage {
           src = self;
+          cargoBuildCommand = "cargo build --features=reqwest";
+          buildInputs = with pkgs; [
+            openssl
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
         };
         packages.default = self.packages.${system}.oura;
       }
